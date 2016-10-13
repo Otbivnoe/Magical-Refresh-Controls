3c305ae2ad5681f1fa4b66d7070a7b92a05c3c6d
Nikita <nikita.ermolenko@rosberry.com>
2016-10-13 20:37:24 +0600

    Init commit

---
+++ MagicalRefreshControls/MagicalRefreshControls/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  MagicalRefreshControls
+//
+//  Created by Nikita on 13/10/2016.
+//  Copyright © 2016 Nikita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
